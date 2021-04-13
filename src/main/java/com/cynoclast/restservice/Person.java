a86f8329950aa10b797211cd50ec55f4ac5c73fd
cynoclast <cynoclat@gmail.com>
2021-04-13 13:21:28 -0700

    commit 0

---
+++ src/main/java/com/cynoclast/restservice/Person.java
@@ -0,0 +1,44 @@
+package com.cynoclast.restservice;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String firstName;
+    private String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SIMPLE_STYLE).
+                append("firstName", firstName).
+                append("lastName", lastName).
+                toString();
+    }
+}
